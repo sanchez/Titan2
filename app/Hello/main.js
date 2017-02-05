a115ebc09710aa2d0417e999fe8d508860289dd2
Daniel Fitz <dfitz360@gmail.com>
2017-02-05 12:58:27 +1000

    Fixed loading error

--- app/Hello/main.js
+++ app/Hello/main.js
@@ -2,7 +2,7 @@
 module.exports = {
     load: function() {
         require("lib/logger").debug("Loaded Package");
-    }
+    },
 
     unload: function() {
         require("lib/logger").debug("UnLoaded Package");
