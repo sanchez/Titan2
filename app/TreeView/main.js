2ffe10ac5c64c2580a5f0101d0a3e10703d8bc05
Daniel Fitz <dfitz360@gmail.com>
2017-02-06 21:25:38 +1000

    Started work on restarting

--- app/TreeView/main.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    activate: function() {
-        require("lib/logger").debug("Setting up TreeView");
-        var win = require("lib/window").getWindow();
-        win.setTreeView("Hello");
-    }
-}
