15998de6a56381a99ecea8ee230e0bb82aade11a
Daniel Fitz <dfitz360@gmail.com>
2017-02-07 16:59:22 +1000

    Added comments

--- src/packages/logger/logger.js
+++ src/packages/logger/logger.js
@@ -1,4 +1,5 @@
 
+// Creates access to the stack trace for printing out the calling function
 Object.defineProperty(global, '__stack', {
     get: function() {
         var orig = Error.prepareStackTrace;
@@ -13,18 +14,21 @@ Object.defineProperty(global, '__stack', {
     }
 });
 
+// Used for easily accessing the caller line
 Object.defineProperty(global, '__line', {
     get: function() {
         return __stack[3].getLineNumber();
     }
 });
 
+// Used for easily accessing the caller filename
 Object.defineProperty(global, '__file', {
     get: function() {
         return __stack[3].getFileName();
     }
 });
 
+// Gets the timestamp for printing the time to the console
 var getTimeStamp = () => {
     var date = new Date();
 
@@ -38,16 +42,22 @@ var getTimeStamp = () => {
     return "[" + hour + ":" + minute + ":" + sec + "]";
 }
 
+// Gets the file and line number stamp for debug output
 var getDebugStamp = () => {
     var fileName = __file;
     fileName = fileName.slice(fileName.lastIndexOf('/') + 1);
     return `(${fileName}:${__line})`
 }
 
+/**
+ * Prints the message out to the console with the time and file stamp
+ * @param  {String} message The message that needs to be printed the the console
+ */
 var log = (message) => {
     console.log(getTimeStamp() + getDebugStamp() + "  " + message);
 }
 
+// Creates a global logger instance so that logger can be accessed globally throughout the whole application
 Object.defineProperty(global, "logger", {
     get: () => {
         return {
@@ -56,6 +66,7 @@ Object.defineProperty(global, "logger", {
     }
 });
 
+// Defines the functions to be exported
 module.exports = {
     log
 }
