622a45b7bb02140b61f8cd6c4ac83d70fa8493e8
Daniel Fitz <dfitz360@gmail.com>
2017-06-30 21:06:26 +1000

    Added status bar and root container

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
 'use strict';
 
 process.env.NODE_PATH = __dirname;
-require('module').Module._initPaths();
+require("module").Module._initPaths();
 
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-var mainWindow = null;
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+app.on('ready', () => {
+    window = require("lib/window").getWindow();
 
-    var logger = require("lib/logger.js");
-    logger.setLogLevel(logger.DEBUG);
-
-    var packages = require("lib/packages.js");
-    console.log(packages.getPackages());
-    packages.loadPackages(packages.getPackages());
-
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
+    window.BrowserWindow.on("closed", () => {
+        window.BrowserWindow = null;
+    })
 });
